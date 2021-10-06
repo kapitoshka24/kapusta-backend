891196452409530b90fb6f26725a8a7c84c1925f
Fane <morenecba@gmail.com>
2021-10-07 01:45:26 +0300

    update end-point summary Improved the code
    улучшил качество кода

--- controllers/getSummary.js
+++ controllers/getSummary.js
@@ -1,76 +1,60 @@
 const CurrencyMovement = require('../model/currencyMovement');
 
-const { incomes } = require('../helpers/categories');
+const { expends } = require('../helpers/categories');
+
+const months = [
+  'Январь',
+  'Февраль',
+  'Март',
+  'Апрель',
+  'Май',
+  'Июнь',
+  'Июль',
+  'Август',
+  'Сентябрь',
+  'Октябрь',
+  'Ноябрь',
+  'Декабрь',
+];
 
 const getSummary = async (req, res) => {
-  const months = [
-    'Январь',
-    'Февраль',
-    'Март',
-    'Апрель',
-    'Май',
-    'Июнь',
-    'Июль',
-    'Август',
-    'Сентябрь',
-    'Октябрь',
-    'Ноябрь',
-    'Декабрь',
-  ];
-
-  const summaryIncome = [];
-
-  for (let i = 1; i < 13; i++) {
-    const monthlyExpenses = await CurrencyMovement.aggregate([
-      {
-        $project: {
-          _id: 1,
-          year: { $year: '$createdAt' },
-          month: { $month: '$createdAt' },
-          day: { $dayOfMonth: '$createdAt' },
-          category: {
-            $in: ['$category', incomes],
-          },
-          sum: 1,
+  const response = await CurrencyMovement.aggregate([
+    {
+      $match: {
+        date: {
+          $gte: new Date('2021-01-01'),
+          $lt: new Date('2022-01-01'),
         },
       },
-      {
-        $group: {
-          _id: {
-            year: '$year',
-            month: '$month',
-            day: '$day',
-            category: '$category',
-          },
-          sum: { $sum: '$sum' },
+    },
+    {
+      $project: {
+        date: '$date',
+        sum: '$sum',
+        category: {
+          $in: ['$category', expends],
         },
       },
-      { $match: { '_id.year': 2021, '_id.month': i, '_id.category': true } },
-      {
-        $group: {
-          _id: i,
-          sum: { $sum: '$sum' },
+    },
+    { $match: { category: true } },
+    {
+      $group: {
+        _id: {
+          $dateToString: { format: '%m', date: '$date' },
         },
+        total: { $sum: '$sum' },
       },
-    ]);
+    },
+  ]);
 
-    const month = i;
-
-    const updatedArray = {
-      month: months[month - 1],
-      sum: 0,
-      ...monthlyExpenses[0],
-    };
-    delete updatedArray._id;
-
-    summaryIncome.push(updatedArray);
+  for (let i in response) {
+    response[i]._id = months[response[i]._id - 1];
   }
-  console.log(summaryIncome);
 
   res.status(200).json({
     status: 'success',
     code: '202',
-    result: summaryIncome,
+    result: response,
   });
 };
 
